1e10d3dfe5973d480894f769090413ebec103b52
Allen <allen0321.lin@ehsn.com.tw>
2020-10-08 17:33:07 +0800

    添加 Action Addon

--- src/components/ChakraButton/ChakraButton.stories.js
+++ src/components/ChakraButton/ChakraButton.stories.js
@@ -1,24 +1,30 @@
 ﻿import React from "react";
 import { Button } from "@chakra-ui/core";
+import { action, actions } from "@storybook/addon-actions";
 
 export default {
   title: "Chakra/Button",
   component: Button,
+  argTypes: {
+    variantColor: { control: "text" },
+    children: { control: "text" },
+    /*all click handler*/
+    // onClick: { action: "clicked" },
+  },
 };
 
-const Template = args => {
-  const { content, ...rest } = args;
-  return <Button {...rest}>{content}</Button>;
-};
+const Template = args => <Button {...args} />;
 
 export const Success = Template.bind({});
 export const Danger = Template.bind({});
 Success.args = {
   variantColor: "green",
-  content: "Success",
+  children: "Success",
+  onClick: action("Click Event"),
 };
 
 Danger.args = {
   variantColor: "red",
-  content: "Danger",
+  children: "Danger",
+  ...actions("onClick", "onMouseOver"),
 };
